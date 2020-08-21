0123bed6774e6175b97b0c039f58f604d67b5eaf
developerep2019 <developerep2019@gmail.com>
2020-08-22 02:59:40 +0600

    shopping cart complete

--- app.js
+++ app.js
@@ -1,12 +1,11 @@
 // phone & case Button event Handler
 function handleProductChange(product, isIncrese , multiplyValue) {
-    const productCount = document.getElementById( product + 'QuantityValue').value;
-    const convertProductCount = parseFloat(productCount);
+    const convertProductCount = getInputValue(product);
     let presentProductQuantity = convertProductCount;
     if(isIncrese == true) {
         presentProductQuantity = convertProductCount + 1;
     }
-    if(isIncrese == false && productCount > 0) {
+    if(isIncrese == false && convertProductCount > 0) {
         presentProductQuantity = convertProductCount - 1;
     }
     document.getElementById(product + 'QuantityValue').value = presentProductQuantity;
@@ -18,4 +17,24 @@ function handleProductChange(product, isIncrese , multiplyValue) {
         productTotal = presentProductQuantity * 59;
     }
     document.getElementById(product + 'TotalPrice').innerText = productTotal;
+
+    // Connection of subtotal and separate product function
+    calculateTotal()
+}
+
+//Subtotal Handler
+function getInputValue(product) {
+    const productInput = document.getElementById( product + 'QuantityValue').value;
+    const productCount = parseFloat(productInput);
+    return productCount;
+}
+
+function calculateTotal() {
+    const phoneCount = getInputValue('phone') * 1219;
+    const caseCount = getInputValue('case') * 59;
+    const totalCount = phoneCount + caseCount;
+    document.getElementById('subtotalPrice').innerText = totalCount;
+    const tax = Math.round(totalCount * 0.1);
+    document.getElementById('taxAmount').innerText = tax;
+    document.getElementById('grandTotal').innerText = totalCount + tax;
 }
